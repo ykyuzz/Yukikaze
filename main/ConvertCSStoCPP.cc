7f2aef0a6abfe116648ac3ac606ee216f7f0c5cd
Y_KUNIEDA <kyasuhiro9@gmail.com>
2024-02-12 23:27:12 +0900

    greatworks

---
+++ main/ConvertCSStoCPP.cc
@@ -0,0 +1,8 @@
+/*
+This program was coded to convert a CSS code to a C++ code to decorate letters in command line or command prompt. 
+This program follows guideline of CSS level1 by W3C
+*/
+
+#include <stdio.h>
+#include <iostream>
+
